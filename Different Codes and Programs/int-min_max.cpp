cda4375bb1ec2fc2c82350c3e461ace061f8508b
Maaz-Code <500077097@stu.upes.ac.in>
2021-02-21 00:38:59 +0530

    Updation

---
+++ Different Codes and Programs/int-min_max.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"Enter maximum number of elements in the array: ";
+    cin >> n;
+    int arr[n];
+    cout<<"Elements of the array are: ";
+    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int maxNo = INT_MIN;
+    int minNo = INT_MAX;
+
+    for(int j=0;j<n;j++){ 
+        maxNo = max(maxNo,arr[j]);
+        minNo = min(minNo,arr[j]);
+    }
+    cout<<"Maximum number is: "<<maxNo<<endl;
+    cout<<"Minimum number is: "<<minNo<<endl;
+    return 0;
+}
