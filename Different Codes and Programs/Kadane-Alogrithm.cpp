963fe63281b9eced70bb010b674beef0d2615b66
Maaz-Code <500077097@stu.upes.ac.in>
2021-03-04 00:52:09 +0530

    Updated notes

--- Different Codes and Programs/Kadane-Alogrithm.cpp
+++ Different Codes and Programs/Kadane-Alogrithm.cpp
@@ -5,6 +5,8 @@ using namespace std;
 //This algorithm is used to reduce the time complexity of the problem where we have to find the sum of all elements of the subarray having the largest sum.
 //The time complexity gets reduced from O(n^3) to O(n) as 3 for loops reduces to one.
 
+
+//This is for O(n):
 int main() {
 
     int n;
@@ -21,11 +23,25 @@ int main() {
     int maxSum = INT_MIN;
 
     for(int i=0;i<n;i++){
-        currentSum += arr[i];
-        if(currentSum<0){
+        currentSum += arr[i]; // add the subsequent numbers
+        if(currentSum<0){ //if the sum becomes -ve then we discard the previous elements and make currentSum = 0
             currentSum = 0;
         }
-        maxSum = max(currentSum,arr[i]);
+        maxSum = max(maxSum,currentSum); //take out the maximum one between them
     }
     cout<<"Maximum sum of the elements from a subarray among all other subarrays having the largest sum: "<<maxSum;
 }
+
+
+//for O(n^3):
+//after getting all elements for array
+//int maxSum=0
+//for(int i=0;i<n;i++){
+//     for(int j=i;j<n;j++){
+//         int sum=0;
+//         for(int k=i;k<=j;k++){
+//             sum = sum + arr[k];
+//         }
+//         maxSum= max(maxSum,sum);
+//     }
+// }
